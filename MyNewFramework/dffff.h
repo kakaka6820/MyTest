9a8c08ad7bd13b9ca3f308f636cc776b72e16f63
mengqingfeng <541655626@qq.com>
2018-09-09 22:29:45 +0800

    new test

---
+++ MyNewFramework/dffff.h
@@ -0,0 +1,13 @@
+//
+//  dffff.h
+//  MyNewFramework
+//
+//  Created by mengqingfeng on 2018/9/9.
+//  Copyright © 2018年 mengqingfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dffff : UILabel
+
+@end
---
+++ MyNewFramework/dffff.m
@@ -0,0 +1,21 @@
+//
+//  dffff.m
+//  MyNewFramework
+//
+//  Created by mengqingfeng on 2018/9/9.
+//  Copyright © 2018年 mengqingfeng. All rights reserved.
+//
+
+#import "dffff.h"
+
+@implementation dffff
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
